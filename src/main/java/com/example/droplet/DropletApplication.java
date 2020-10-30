8d0175e902fd812740105d42e79a26f124fde4d3
elivar <elargor@gmail.com>
2020-10-30 17:26:24 -0500

    mi primer comit

---
+++ src/main/java/com/example/droplet/DropletApplication.java
@@ -0,0 +1,13 @@
+package com.example.droplet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DropletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/droplet/DropletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.droplet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
